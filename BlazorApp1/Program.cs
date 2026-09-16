0bac7db2ead404a4c162b9135268cb42dfcd89fd
Kazuki Ota <117221407+kaota_microsoft@users.noreply.github.com>
2026-09-16 14:11:50 +0900

    プロジェクトファイルを追加。

---
+++ BlazorApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using BlazorApp1.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+app.UsePathBase("/myapp");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>();
+
+app.Run();
---
+++ BlazorApp1/ViewModels/PersonViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.ViewModels;
+
+public class PersonViewModel
+{
+    [Display(Name = "名")]
+    public string FirstName { get; set; } = "";
+
+    [Display(Name = "姓")]
+    public string LastName { get; set; } = "";
+}
