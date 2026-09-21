585f64d06d0adff0f1131ec79174057ca2189154
Kazuki Ota <117221407+kaota_microsoft@users.noreply.github.com>
2026-09-21 12:34:44 +0900

    a

---
+++ BlazorApp1/ViewModels/SSRFormValidationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.ViewModels;
+
+public class SSRFormValidationViewModel
+{
+    [Display(Name = "名前")]
+    [Required(ErrorMessage = "名前は必須です。")]
+    public string Name { get; set; } = "";
+
+    [Display(Name = "説明")]
+    [Required(ErrorMessage = "説明は必須です。")]
+    [MinLength(10, ErrorMessage = "説明は10文字以上で入力してください。")]
+    public string Description { get; set; } = "";
+}
